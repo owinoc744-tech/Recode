c1ffc527367d4489ceeff5a19725633ba4c855e2
owinoc744-tech <owinoc744@gmail.com>
2026-07-23 00:00:50 +0300

    vars in go

--- golang/var.go
+++ golang/var.go
@@ -1,8 +1,12 @@
-package main 
+package main
+// package main maens this is an executable program 
 import "fmt"
+// import  allows us to utilize go packages 
 func main(){
-
+// funcmain this is where we start to execute  our code
 var name string = "Collins"
+// var store data of different types
 fmt.Printf("My name is %v i am a and i am a dev \n ",name)
-
+fmt.Println(&name)
+// &name this lets us print the address where name is stored
 }
