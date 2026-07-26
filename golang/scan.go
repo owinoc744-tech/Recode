6aa02398f3f43339a35ebbdc81b2a8bf8d61a0c0
owinoc744-tech <owinoc744@gmail.com>
2026-07-26 20:38:09 +0300

    love

--- golang/scan.go
+++ golang/scan.go
@@ -1,11 +1,14 @@
 package main
-import "fmt"
+import (
+        "fmt"
+        
+         )
 func main(){
 
 var name string
 var id int
 fmt.Println("Enter your name")
-fmt.Scan(&name)
+fmt.Scan(&name) 
 fmt.Println("Enter your id number")
 fmt.Scan(&id)
 fmt.Printf("Welcome to techinsigt %s of idnumber %v \n",name,id)
