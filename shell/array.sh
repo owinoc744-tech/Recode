33b9554f16d26d423641c1939e2c2ea8b8e361b2
owinoc744-tech <owinoc744@gmail.com>
2026-09-04 08:41:43 +0300

    shell arrays

---
+++ shell/array.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+name=(allan berry)
+echo "${name}"
+echo "${name[1]}"
+echo "${#name[@]}"
---
+++ shell/array1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+nam=(allan berry carl derril erick)
+echo "${nam}"
+echo  "${nam[1]}"
+echo   "${nam[3]}"
+echo   "length of array" "${#nam[@]}" 
